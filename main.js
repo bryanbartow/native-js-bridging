3c64ce271e81f099da3dfbb534d4e36e3cf52412
Bryan Bartow <bryanbartow@gmail.com>
2020-01-27 17:08:22 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-function sendLoginAction() {
+function sendIosAction() {
 	try {
-	    webkit.messageHandlers.loginAction.postMessage(
-	    	document.getElementById("email").value + " " + document.getElementById("password").value
-	    );
-    } catch(err) {
-        console.log('The native context does not exist yet');
-    }
+		webkit.messageHandlers.registrationSucceeded.postMessage(
+	    		"success"
+	    	);
+    	} catch(err) {
+        	console.log('The native context does not exist yet');
+    	}
 }
 
-function mobileHeader() {
-    document.querySelector('h1').innerHTML = "WKWebView Mobile";
+function sendAndroidAction() {
+	//
 }
